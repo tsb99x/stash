d53fae2e4055dd1f9025810b09d2d10fcc0ce04a
Anton Muravyov <tsb99x@gmail.com>
2019-03-04 21:52:36 +0300

    Use std ns out-of-box, Place includes in source

--- culling-numbers/main.cpp
+++ culling-numbers/main.cpp
@@ -1,33 +1,35 @@
 #include "main.h"
 
-using namespace std::string_literals;
+#include <string>
+
+using namespace std;
 
 int main(const int argc, const char *argv[])
 {
     try {
         auto args = convert_args(argc, argv);
         auto culled = cull(args);
-        output(culled, std::cout);
-    } catch (const std::runtime_error &e) {
-        std::cerr << e.what();
+        output(culled, cout);
+    } catch (const runtime_error &e) {
+        cerr << e.what();
     }
 }
 
-std::vector<int> convert_args(const int argc, const char *argv[])
+vector<int> convert_args(const int argc, const char *argv[])
 {
     if (argc < 2) {
-        throw std::runtime_error("Failed to get arguments");
+        throw runtime_error("Failed to get arguments");
     }
 
-    std::vector<int> vec;
+    vector<int> vec;
 
     for (int i = 1; i < argc; i++) {
         int arg;
 
         try {
-            arg = std::stoi(argv[i]);
-        } catch (const std::invalid_argument &ia) {
-            throw std::runtime_error("Failed to process invalid argument of '"s + argv[i] + "'");
+            arg = stoi(argv[i]);
+        } catch (const invalid_argument &ia) {
+            throw runtime_error("Failed to process invalid argument of '"s + argv[i] + "'");
         }
 
         vec.push_back(arg);
@@ -36,13 +38,13 @@ std::vector<int> convert_args(const int argc, const char *argv[])
     return vec;
 }
 
-std::vector<int> cull(const std::vector<int> &args)
+vector<int> cull(const vector<int> &args)
 {
-    std::vector<int> res;
-    std::unordered_set<int> unique;
+    vector<int> res;
+    unordered_set<int> unique;
 
     for (const int arg : args) {
-        if (!contains(unique, arg)) {
+        if (unique.find(arg) == unique.end()) { // If doesn't contain
             unique.insert(arg);
             res.push_back(arg);
         }
@@ -51,12 +53,7 @@ std::vector<int> cull(const std::vector<int> &args)
     return res;
 }
 
-bool contains(const std::unordered_set<int> &container, const int element)
-{
-    return container.find(element) != container.end();
-}
-
-void output(const std::vector<int> &vec, std::ostream &stream)
+void output(const vector<int> &vec, ostream &stream)
 {
     if (vec.empty()) {
         return;
--- culling-numbers/main.h
+++ culling-numbers/main.h
@@ -1,16 +1,13 @@
 #ifndef CULLING_NUMBERS_MAIN_H
 #define CULLING_NUMBERS_MAIN_H
 
-#include <iostream>
 #include <vector>
-#include <string>
 #include <unordered_set>
+#include <iostream>
 
 std::vector<int> convert_args(int argc, const char *argv[]);
 
 std::vector<int> cull(const std::vector<int> &args);
-
-bool contains(const std::unordered_set<int> &container, int element);
 
 void output(const std::vector<int> &vec, std::ostream &stream);
 
