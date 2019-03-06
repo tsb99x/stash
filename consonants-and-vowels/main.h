81e133abb9d4d90be2aa31e8460f722404689ffe
Anton Muravyov <tsb99x@gmail.com>
2019-03-06 22:28:55 +0300

    Cleanup header macros

--- consonants-and-vowels/main.h
+++ consonants-and-vowels/main.h
@@ -1,8 +1,8 @@
-#ifndef CONSONANTS_AND_VOWELS_C_MAIN_H
-#define CONSONANTS_AND_VOWELS_C_MAIN_H
+#ifndef CONSONANTS_AND_VOWELS_MAIN_H
+#define CONSONANTS_AND_VOWELS_MAIN_H
 
 void panic(const char *msg);
 
 char gen_char(char ch);
 
-#endif //CONSONANTS_AND_VOWELS_C_MAIN_H
+#endif //CONSONANTS_AND_VOWELS_MAIN_H
