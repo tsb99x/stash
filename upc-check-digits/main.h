e6e16b8a15c8960ed677b8f58ae7292667af1809
Anton Muravyov <tsb99x@gmail.com>
2019-03-06 22:36:45 +0300

    Implement upc check digits

---
+++ upc-check-digits/main.h
@@ -0,0 +1,8 @@
+#ifndef UPC_CHECK_DIGITS_MAIN_H
+#define UPC_CHECK_DIGITS_MAIN_H
+
+void panic(const char *const msg);
+
+int upc(const char *const str, const size_t pad_size);
+
+#endif // UPC_CHECK_DIGITS_MAIN_H
