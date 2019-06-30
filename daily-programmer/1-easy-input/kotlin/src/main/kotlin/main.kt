95892c85fd462baea6ecad6a58306d2a0c6eeea5
Anton Muravyov <tsb99x@gmail.com>
2019-06-30 22:10:00 +0300

    Implement 1-easy-input in Kotlin

---
+++ daily-programmer/1-easy-input/kotlin/src/main/kotlin/main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    print("Input name > ")
+    val name = readLine()
+
+    print("Input years > ")
+    val years = readLine()
+
+    print("Input username > ")
+    val username = readLine()
+
+    println("Your name is $name, you are $years years old, and your username is $username")
+}
